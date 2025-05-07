bef85aeed04a4655172f359db1c85f99e5225ee4
DevEmontes <erick_montes@americana.edu.co>
2025-05-07 09:32:13 -0500

    servicio de paginacion, obtener usuarios, obtener un solo usuario por id, actualizar usuarios

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
-export class ProductsService extends PrismaClient implements ProductsService {
+export class ProductsService extends PrismaClient implements OnModuleInit {
 
   private readonly logger = new Logger('ProductsService');
 
@@ -21,16 +22,41 @@ export class ProductsService extends PrismaClient implements ProductsService {
     });
   }
 
-  findAll() {
-    return this.product.findMany();
-  }
-  
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findAll( paginationDto: PaginationDto ) {
+
+    const { page = 1 , limit = 10 } = paginationDto;
+    const totalProducts = await this.product.count();
+    const lastPage = Math.ceil(totalProducts / limit);
+
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit
+      }),
+      meta: {
+        Page: page,
+        Limite: limit,
+        totalProductos: totalProducts,
+        lastPage: lastPage
+      }
+    }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where : {id}
+    });
+    if(!product) throw new NotFoundException('Product not found');
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+    return this.product.update({
+      where: { id },
+      data: updateProductDto
+    });
   }
 
   remove(id: number) {
